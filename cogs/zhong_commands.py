ae44fea357a73784a8343039dcbe4ff6ba663d1e
alifiras.py <alifiras.py@gmail.com>
2023-09-02 22:46:17 +0300

    added views

--- cogs/zhong_commands.py
+++ cogs/zhong_commands.py
@@ -1,14 +1,17 @@
 from discord.ext import commands
 from discord import app_commands, Interaction, Embed
 
+from utils.views import WikiView
+
 
 class ZhongCommands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @app_commands.command(description="...")
-    async def learn(self, interaction: Interaction):
-        await interaction.response.send_message(embed=Embed(title="Learning...", color=self.bot.green))
+    @app_commands.command(description="Lists characters and common phrases")
+    async def wiki(self, interaction: Interaction, index: int=1):
+        embed = Embed(title="Learning...", color=self.bot.green)
+        await interaction.response.send_message(embed=embed, view=WikiView())
 
 
 async def setup(bot: commands.Bot):
