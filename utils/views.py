bb75cff25cebd8539406a70c2135906358d95ae4
alifiras.py <alifiras.py@gmail.com>
2023-09-02 22:50:20 +0300

    fixed buttons

--- utils/views.py
+++ utils/views.py
@@ -7,10 +7,10 @@ class WikiView(View):
     def __init__(self, *, timeout=120.0):
         super().__init__(timeout=timeout)
 
-    @button(style=ButtonStyle.blurple, custom_id="previous", emoji=":arrow_left:")
+    @button(style=ButtonStyle.blurple, custom_id="previous", emoji="⬅️")
     async def previous(self, interaction: Interaction, button: Button):
         await interaction.response.send_message("goin left")
 
-    @button(style=ButtonStyle.blurple, custom_id="next", emoji=":arrow_right:")
+    @button(style=ButtonStyle.blurple, custom_id="next", emoji="➡️")
     async def next(self, interaction: Interaction, button: Button):
         await interaction.response.send_message("goin right")
