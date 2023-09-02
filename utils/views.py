68d23fb6536a69d1ccacf5cba699aa52e5c0b462
alifiras.py <alifiras.py@gmail.com>
2023-09-02 23:07:48 +0300

    views

--- utils/views.py
+++ utils/views.py
@@ -1,16 +1,29 @@
 from discord.ui import View, Button, button
-from discord import ButtonStyle, Interaction
+from discord import ButtonStyle, Interaction, Embed
 from discord.ext.commands import Bot
 
 
 class WikiView(View):
-    def __init__(self, *, timeout=120.0):
+    def __init__(self, *, page: int, timeout=120.0):
+        self.page = page
+        self.chars = ['a', 'b', 'c', 'd']
         super().__init__(timeout=timeout)
+
+    async def update(self, interaction: Interaction):
+        embed = Embed(title=self.chars[self.page])
+        if self.page == 1:
+            embed.set_footer(text="Hint: you can jump to any page by choosing an index (ex: /wiki 42)")
+            
+        await interaction.message.edit(embed=embed)
 
     @button(style=ButtonStyle.blurple, custom_id="previous", emoji="⬅️")
     async def previous(self, interaction: Interaction, button: Button):
+        self.page = (self.page - 1) % len(self.chars)
         await interaction.response.send_message("goin left")
+        await self.update(interaction)
 
     @button(style=ButtonStyle.blurple, custom_id="next", emoji="➡️")
     async def next(self, interaction: Interaction, button: Button):
+        self.page = (self.page - 1) % len(self.chars)
         await interaction.response.send_message("goin right")
+        await self.update(interaction)
