970cf671e20684ab60758e57e62948a7022c5f45
alifiras.py <alifiras.py@gmail.com>
2023-09-02 22:10:34 +0300

    coding.

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 """ZhongWenbot Configuraiton"""
 
+
+TOKEN: str = "MTExMDMwODE4MzUzODQyMTg1MA.Gu7lXj.Z4snJVtSrwK5PRK6SfvRkrBZ6u1gxjhuMYJIqw"
 prefix: str = "z!"
-TOKEN: str = ""
-owner_id: int = 0
+owner_id: int = 424655090108989440
+
 
 colors: dict[str] = {  # RGB values for embed colors
     "red": (240, 10, 10),
